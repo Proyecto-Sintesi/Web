719d4e5e6313ef3c9f68633433f8f3c359d3113c
akex06 <alessandro@akex.dev>
2024-04-03 18:46:14 +0200

    asda

---
+++ main.py
@@ -0,0 +1,12 @@
+from quart import Quart, render_template
+
+app = Quart(__name__)
+
+
+@app.route("/")
+async def home():
+    return await render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(port=5050, debug=True)
